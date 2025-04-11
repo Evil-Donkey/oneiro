4fde6e54a0dc7cdc1563be918e1a0dab234b3b3f
Jeff Ceriello <jeffceriello@gmail.com>
2025-04-11 18:01:41 +0200

    fix centred copy component

--- components/FlexibleContent/CentredCopy/index.js
+++ components/FlexibleContent/CentredCopy/index.js
@@ -4,7 +4,7 @@ const CentredCopy = ({ data }) => {
     const { copy, heading, lightBackground } = data;
 
     return (
-        <div className={`${lightBackground ? `bg-white-01` : `bg-blue-02`} py-20`}>
+        <div className={`${lightBackground ? `bg-white-01 text-blue-02` : `bg-blue-02 text-white-00`} py-20`}>
             <div className="container mx-auto relative px-6 md:px-4">
                 <div className="flex justify-center">
                     <div className="w-1/2 text-center">
