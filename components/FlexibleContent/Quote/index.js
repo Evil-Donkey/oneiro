c6e5f462f0bd1be79a4eb96b41cd3b56857be8ee
Jeff Ceriello <jeffceriello@gmail.com>
2025-05-23 13:00:15 +0200

    Update Quote image gradient overlay

--- components/FlexibleContent/Quote/index.js
+++ components/FlexibleContent/Quote/index.js
@@ -18,7 +18,7 @@ const Quote = ({ data }) => {
         <div className={`${styles.quote} bg-white-01 text-blue-02 lg:text-white`}>
             <div className='container mx-auto relative px-6 md:px-4'>
                 <div className={`${styles.quoteContent} flex flex-col md:flex-row ${fullWidthOverlay ? 'justify-center text-center items-end' : 'justify-end'} p-5 lg:p-20 bg-grey-03 rounded-lg space-x-10 bg-cover bg-left lg:min-h-200 relative overflow-hidden`} style={{ backgroundImage: `url(${image.mediaItemUrl})` }}>
-                    <div className={`hidden lg:block absolute top-0 left-0 w-full h-full ${fullWidthOverlay ? 'bg-black/50' : 'bg-gradient-to-r from-transparent to-black'}`} />
+                    <div className={`hidden lg:block absolute top-0 left-0 w-full h-full ${fullWidthOverlay ? 'bg-gradient-to-t from-black/70 to-transparent' : 'bg-gradient-to-r from-transparent to-black'}`} />
                     {image && <div className="w-full lg:hidden m-0">
                         <LazyItem className="h-full mb-5 md:mb-0">
                             <Image className="h-full overflow-hidden rounded-lg object-cover md:object-[20%_50%]" src={image.mediaItemUrl} alt={image.altText} width={image.mediaDetails.width} height={image.mediaDetails.height} />
