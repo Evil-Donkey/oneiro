faac78f2d7839d84dffe1d4e1d44ab0bde737c0e
Jeff Ceriello <jeffceriello@gmail.com>
2026-04-23 14:23:51 +0200

    add Sopro Outbase

--- app/layout.js
+++ app/layout.js
@@ -50,6 +50,19 @@ export default function RootLayout({ children }) {
         <Footer themeSettings={themeSettings} />
         <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
         <Script src='https://cdn-cookieyes.com/client_data/5a5b8dc6bc1903b2ec2cec59/script.js' strategy='beforeInteractive' />
+        <Script
+          id='sopro-outbase'
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `(function (o, u, t, b, a, s, e, p) {
+              window[b] = window[b] || {}; (e = e || [])['key'] = o; e['__obr'] = u.location.href; e['__obrf'] = u.referrer || 0; a = [];
+              u.location.search.replace('?', '').split('&').forEach(function (q) { if (q.startsWith(b) || q.startsWith('_obid')) e[q.split('=')[0]] = q.split('=')[1]; });
+              e['_obid'] = e['_obid'] || (u.cookie.match(/(^|;)\\s*_obid\\s*=\\s*([^;]+)/) || []).pop() || 0;
+              for (k in e) { if (e.hasOwnProperty(k)) a.push(encodeURIComponent(k) + '=' + encodeURIComponent(e[k])); }
+              s = u.createElement('script'); s.async = true; s.src = t + '?' + a.join('&'); p = u.getElementsByTagName('script')[0]; p.parentNode.insertBefore(s, p);
+            })('a71f4dc9-cfa4-4fd8-b129-b3126d0278be', document, 'https://plugin.sopro.io/hq.js', 'outbase')`,
+          }}
+        />
       </body>
     </html>
   );
